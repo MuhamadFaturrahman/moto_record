e7f41086de7b3eb535e34e7c20ec00cbfca2539d
Muhamad Faturrahman <114564154+MuhamadFaturrahman@users.noreply.github.com>
2023-06-07 20:24:31 +0700

    Adding Detail On Credit Page

--- lib/pages/credit_page.dart
+++ lib/pages/credit_page.dart
@@ -9,6 +9,11 @@ class creditPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('About Us'),
       ),
+      body: const SingleChildScrollView(
+        padding: EdgeInsets.all(16),
+        child: Text(
+            "Eiusmod dolor aliqua duis occaecat duis minim ex ea ex reprehenderit. Occaecat magna nulla minim nulla magna commodo laboris amet quis irure nisi. Fugiat proident anim consequat elit excepteur."),
+      ),
     );
   }
 }
